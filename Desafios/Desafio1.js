20a5d24607ca1871cdf05a29d16d6254db5de9e4
lucas <lucas.dev021@gmail.com>
2023-11-09 10:58:04 -0300

    desafio1.js mudado

--- Desafios/Desafio1.js
+++ Desafios/Desafio1.js
@@ -1,35 +1,47 @@
 // historia do heroi inicial 
-var xpInicial = 1
-console.log(`O jovem heroí q saiu de sua cidade com um xp inicial de ${xpInicial} achou uma caverna magica e nessa caverna encontrou um elixir supremo de XP, tomou
+var perfil = {
+    nomeDoHeroi : "Lucas Vieira Ferrari",
+    definicao : "",
+    xpInicial : 1
+}
+function descobrindoElixir(){
+console.log(`O jovem heroí q saiu de sua cidade com um xp inicial de ${perfil.xpInicial} achou uma caverna magica e nessa caverna encontrou um elixir supremo de XP, tomou
 esse elixir e se sentiu estranho e desmaiou. `)
+
 for (var i = 1 ; i <= 200000 ; i += 100000 ){
-    xpInicial += 100000 
+    perfil.xpInicial += 100000 
+}
 }
 // Desafio de classificação
-var nomeDoHeroi = "Lucas Vieira Ferrari"
-var definicao = ""
-if(xpInicial === 1000){
+function rank(){
+if(perfil.xpInicial === 1000){
     definicao = "Ferro"
 }
-else if (xpInicial >1.001 && xpInicial <=2.000){
+else if (perfil.xpInicial >1.001 && perfil.xpInicial <=2.000){
     definicao = "Bronze"
 }
-else if (xpInicial >2.001 && xpInicial <=6.001){
+else if (perfil.xpInicial >2.001 && perfil.xpInicial <=6.001){
     definicao = "Prata"
 }
-else if(xpInicial >6.000 && xpInicial <=7.000){
+else if(perfil.xpInicial >6.000 && perfil.xpInicial <=7.000){
     definicao = "Ouro"
 }
-else if (xpInicial > 7.001 && xpInicial <=8.000){
+else if (perfil.xpInicial > 7.001 && perfil.xpInicial <=8.000){
     definicao = "Platina"
 }
-else if (xpInicial >8.001  && xpInicial <=9.000){
+else if (perfil.xpInicial >8.001  && perfil.xpInicial <=9.000){
     definicao = "Ascendente"
 }
-else if (xpInicial >9.001  && xpInicial <=10.000){
+else if (perfil.xpInicial >9.001  && perfil.xpInicial <=10.000){
     definicao = "Imortal"
 }
 else {
     definicao = "Radiante"
 }
-console.log(`O Herói de nome ${nomeDoHeroi} acordou, abriu sua hotbar e conferiu que estava na classificação de ${definicao}, percebendo assim que tinha recebido uma incrível quantidade de XP, parando assim no nível ${xpInicial}.`);
+}
+function heroiAtual(){
+console.log(`O Herói de nome ${perfil.nomeDoHeroi} acordou, abriu sua hotbar e conferiu que estava na classificação de ${definicao}, percebendo assim que tinha recebido uma incrível quantidade de XP, parando assim no nível ${perfil.xpInicial}.`);
+}
+descobrindoElixir()
+rank()
+heroiAtual()
