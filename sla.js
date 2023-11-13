751bc73d76ae8dccbdab2c47214bfcbc1b633661
lucas <lucas.dev021@gmail.com>
2023-11-13 19:33:34 -0300

    ainn

---
+++ sla.js
@@ -0,0 +1,11 @@
+let numeroUm= 9
+let numeroDois= 6
+let numeroTres = 6
+let soma = numeroUm + numeroDois + numeroTres
+var total = soma / 3
+if (total >= 7) {
+    console.log("aprovado")
+}
+else {
+    console.log("reprovado")
+}
